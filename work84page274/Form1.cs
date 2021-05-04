4829b7b6dec5d494441f61912420b370a990bfca
Elje <Juayeethao16@gmail.com>
2021-05-04 11:11:45 +0700

    Add project files.

---
+++ work84page274/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace work84page274
+{
+    public partial class Form1 : Form
+    {
+        SoundPlayer p1 = new SoundPlayer("without.wav");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p1.Play();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            p1.Stop();
+        }
+    }
+}
